09ba0e64be440d911646516be0b3e7c0d7b92aad
Martin Voelkle <martin.voelkle@epfl.ch>
2015-10-22 17:26:59 +0200

    use explicit executable extensions

--- build.cmd
+++ build.cmd
@@ -1,18 +1,18 @@
 mkdir build\dashel
 pushd build\dashel
-call cmake -G "MinGW Makefiles" "%WORKSPACE%\source\dashel"
-call make
+cmake.exe -G "MinGW Makefiles" "%WORKSPACE%\source\dashel"
+make.bat
 popd
 
 mkdir build\enki
 pushd build\enki
-call cmake -G "MinGW Makefiles" "%WORKSPACE%\source\enki"
-call make
+cmake.bat -G "MinGW Makefiles" "%WORKSPACE%\source\enki"
+make.bat
 popd
 
 mkdir build\aseba
 pushd build\aseba
-call cmake -G "MinGW Makefiles"^
+cmake.exe -G "MinGW Makefiles"^
  -D "dashel_DIR=%WORKSPACE%\build\dashel"^
  -D "DASHEL_INCLUDE_DIR=%WORKSPACE%\source\dashel"^
  -D "DASHEL_LIBRARY=%WORKSPACE%\build\dashel\libdashel.dll.a"^
@@ -24,15 +24,15 @@ call cmake -G "MinGW Makefiles"^
  -D "QWT_INCLUDE_DIR=%ASEBA_DEP%\qwt\qwt-5.2.1\src"^
  -D "QWT_LIBRARIES=%ASEBA_DEP%\qwt\qwt-5.2.1\lib\libqwt5.a"^
  "%WORKSPACE%\source\aseba"
-call make
+make.bat
 mkdir strip
 for /f %%F in ('dir *.exe /s /b') do (
-	call objcopy --strip-all %%F strip\%%~nxF
+	objcopy.exe --strip-all %%F strip\%%~nxF
 )
 popd
 
 mkdir build\package
 pushd build\package
-call makensis /Oaseba-package.log "/DASEBA_DEP=%ASEBA_DEP%" -- "%WORKSPACE%\source\package\aseba.nsi"
+makensis.exe /Oaseba-package.log "/DASEBA_DEP=%ASEBA_DEP%" -- "%WORKSPACE%\source\package\aseba.nsi"
 move "%WORKSPACE%\source\package\*.exe" .
 popd
