67cb2120313f8cc8b7c84735c1be06365bc4f94d
Martin Voelkle <martin.voelkle@epfl.ch>
2015-10-29 12:45:41 +0100

    build static dashel

--- build.cmd
+++ build.cmd
@@ -1,12 +1,15 @@
 mkdir build\dashel
 pushd build\dashel
-cmake.exe -G "MinGW Makefiles" "%WORKSPACE%\source\dashel"
+cmake.exe -G "MinGW Makefiles"^
+ -D "BUILD_SHARED_LIBS=OFF"^
+ "%WORKSPACE%\source\dashel"
 call make.bat
 popd
 
 mkdir build\enki
 pushd build\enki
-cmake.exe -G "MinGW Makefiles" "%WORKSPACE%\source\enki"
+cmake.exe -G "MinGW Makefiles"^
+ "%WORKSPACE%\source\enki"
 call make.bat
 popd
 
@@ -15,7 +18,7 @@ pushd build\aseba
 cmake.exe -G "MinGW Makefiles"^
  -D "dashel_DIR=%WORKSPACE%\build\dashel"^
  -D "DASHEL_INCLUDE_DIR=%WORKSPACE%\source\dashel"^
- -D "DASHEL_LIBRARY=%WORKSPACE%\build\dashel\libdashel.dll.a"^
+ -D "DASHEL_LIBRARY=%WORKSPACE%\build\dashel\libdashel.a"^
  -D "ENKI_INCLUDE_DIR=%WORKSPACE%\source\enki"^
  -D "ENKI_LIBRARY=%WORKSPACE%\build\enki\enki\libenki.a"^
  -D "ENKI_VIEWER_LIBRARY=%WORKSPACE%\build\enki\viewer\libenkiviewer.a"^
