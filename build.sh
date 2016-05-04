8d40a6005e4e7ff80242abdd040f20f53274045e
davidjsherman <david.sherman@inria.fr>
2016-05-04 20:26:59 +0200

    no ln -s in windows. use libdashel.a not dylib

--- build.sh
+++ build.sh
@@ -4,37 +4,38 @@ echo QWT PATH is ${QWTDIR:='/mingw32'}
 echo ASEBA DEP is ${ASEBA_DEP:='/mingw32/bin'}
 set -eu
 
-[ -d "$WORKSPACE/source" -o -L "$WORKSPACE/source" ] || ln -s . "$WORKSPACE/source"
-
 mkdir -p "$WORKSPACE/build/dashel"
 cd "$WORKSPACE/build/dashel"
 cmake\
+ -G "Unix Makefiles" \
  -D "CMAKE_BUILD_TYPE=Release"\
  -D "BUILD_SHARED_LIBS=OFF"\
- "$WORKSPACE/source/dashel"
+ "$WORKSPACE/dashel"
 make
 
 mkdir -p "$WORKSPACE/build/enki"
 cd "$WORKSPACE/build/enki"
 cmake\
+ -G "Unix Makefiles" \
  -D "CMAKE_BUILD_TYPE=Release"\
  -D "BUILD_SHARED_LIBS=OFF"\
- "$WORKSPACE/source/enki"
+ "$WORKSPACE/enki"
 make
 
 mkdir -p "$WORKSPACE/build/aseba"
 cd "$WORKSPACE/build/aseba"
 cmake\
+ -G "Unix Makefiles" \
  -D "CMAKE_BUILD_TYPE=Release"\
  -D "dashel_DIR=$WORKSPACE/build/dashel"\
- -D "DASHEL_INCLUDE_DIR=$WORKSPACE/source/dashel"\
- -D "DASHEL_LIBRARY=$WORKSPACE/build/dashel/libdashel.dylib"\
- -D "ENKI_INCLUDE_DIR=$WORKSPACE/source/enki"\
+ -D "DASHEL_INCLUDE_DIR=$WORKSPACE/dashel"\
+ -D "DASHEL_LIBRARY=$WORKSPACE/build/dashel/libdashel.a"\
+ -D "ENKI_INCLUDE_DIR=$WORKSPACE/enki"\
  -D "ENKI_LIBRARY=$WORKSPACE/build/enki/enki/libenki.a"\
  -D "ENKI_VIEWER_LIBRARY=$WORKSPACE/build/enki/viewer/libenkiviewer.a"\
  -D "QWT_INCLUDE_DIR=$QWTDIR/include/qwt"\
  -D "QWT_LIBRARIES=$QWTDIR/lib/libqwt.a"\
- "$WORKSPACE/source/aseba"
+ "$WORKSPACE/aseba"
 make
 
 mkdir -p "$WORKSPACE/build/packager"
@@ -42,9 +43,9 @@ cd "$WORKSPACE/build/packager"
 makensis \
     -D"ASEBA_DEP=$ASEBA_DEP" \
     -D"QTDIR=$QTDIR" \
-    -- "$WORKSPACE/source/package/aseba.nsi"
+    -- "$WORKSPACE/package/aseba.nsi"
 
 cd "$WORKSPACE"
-mv "$WORKSPACE/source/package/*.exe" .
+mv "$WORKSPACE/package/*.exe" .
 env
 
